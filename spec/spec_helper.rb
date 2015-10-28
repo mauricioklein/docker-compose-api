cfe6af6be0c81bdad1db856d81a1dcdca45f66c3
Mauricio Klein <mauricio.klein@zenvia.com>
2015-10-28 11:15:51 -0200

    Removing spec/ files from SimpleCov coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
 require 'codeclimate-test-reporter'
 
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 CodeClimate::TestReporter.start
 
 require 'docker-compose'
