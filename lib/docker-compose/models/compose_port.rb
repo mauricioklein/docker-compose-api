d04108807985ef24d8ac89d4186759105ddbd30c
Mauricio Klein <mauricio.klein@jura-online.de>
2017-02-10 17:38:01 +0100

    Module "ComposeUtils" refactored

--- lib/docker-compose/models/compose_port.rb
+++ lib/docker-compose/models/compose_port.rb
@@ -1,9 +1,9 @@
 class ComposePort
   attr_reader :container_port, :host_ip, :host_port
 
-  def initialize(container_port, host_port = nil, host_ip = nil)
-    @container_port = container_port
-    @host_ip = host_ip
-    @host_port = host_port
+  def initialize(params)
+    @container_port = params[:container_port]
+    @host_ip = params[:host_ip]
+    @host_port = params[:host_port]
   end
 end
--- lib/docker-compose/utils/compose_utils.rb
+++ lib/docker-compose/utils/compose_utils.rb
@@ -1,157 +1,114 @@
 module ComposeUtils
-  @dir_name = File.split(Dir.pwd).last.gsub(/[-_]/, '')
-  @current_container_id = nil
-
-  #
-  # Returns the directory name where compose
-  # file is saved (used in container naming)
-  #
-  def self.dir_name
-    @dir_name
-  end
-
-  #
-  # Provides the next available ID
-  # to container names
-  #
-  def self.next_available_id
-    if @current_container_id.nil?
-      # Discovery the max id used by already running containers
-      # (default to '0' if no container is running)
-      @current_container_id = Docker::Container.all(all: true).map {|c| c.info['Names'].last.split(/_/).last.to_i}.flatten.max || 0
+  class << self
+    #
+    # Returns the directory name where compose
+    # file is saved (used in container naming)
+    #
+    def dir_name
+      File.split(Dir.pwd).last.gsub(/[-_]/, '')
     end
 
-    @current_container_id += 1
-  end
-
-  #
-  # Format a given docker image in a complete structure (base image + tag)
-  #
-  def self.format_image(image)
-    base_image = nil
-    tag = nil
-
-    if image.nil?
-      return nil
+    #
+    # Provides the next available ID
+    # to container names
+    #
+    def next_available_container_id
+      get_container_id = -> (container) { container.info['Names'].last.split(/_/).last.to_i }
+      current_container_id = Docker::Container.all(all: true).map { |c| get_container_id(c) }.flatten.max || 0
+      current_container_id + 1
     end
 
-    if image.index(':').nil?
-      base_image = image
-      tag = 'latest'
-    else
-      image_split = image.split(':')
-      base_image = image_split[0]
-      tag = image_split[1]
+    #
+    # Format a given docker image in a complete structure (base image + tag)
+    #
+    def format_image(image)
+      return nil if image.nil?
+      image.include?(?:) ? image : "#{image}:latest"
     end
 
-    "#{base_image}:#{tag}"
-  end
-
-  #
-  # Transform docker command from string to an array of commands
-  #
-  def self.format_command(command)
-    command.nil? ? nil : command.split(' ')
-  end
-
-  #
-  # Read a port specification in string format
-  # and create a compose port structure
-  #
-  def self.format_port(port_entry)
-    compose_port = nil
-    container_port = nil
-    host_port = nil
-    host_ip = nil
-
-    port_parts = port_entry.split(':')
-
-    case port_parts.length
-      # [container port]
-      when 1
-        compose_port = ComposePort.new(port_parts[0])
-
-      # [host port]:[container port]
-      when 2
-        compose_port = ComposePort.new(port_parts[1], port_parts[0])
-
-      # [host ip]:[host port]:[container port]
-      when 3
-        compose_port = ComposePort.new(port_parts[2], port_parts[1], port_parts[0])
+    #
+    # Transform docker command from string to an array of commands
+    #
+    def format_command(command)
+      command && command.split(' ')
     end
 
-    compose_port
-  end
+    #
+    # Read a port specification in string format
+    # and create a compose port structure
+    #
+    def format_port(port_entry)
+      port_parts = port_entry.split(':').reverse
 
-  #
-  # Format ports from running container
-  #
-  def self.format_ports_from_running_container(port_entry)
-    entries = []
-    container_port = nil
-    host_ip = nil
-    host_port = nil
-
-    if port_entry.nil?
-      return entries
-    end
-
-    port_entry.each do |key, value|
-      container_port = key.gsub(/\D/, '').to_i
-      # Ports that are EXPOSEd but not published won't have a Host IP/Port,
-      # only a Container Port.
-      if value.nil?
-        host_ip = ''
-        host_port = ''
-      else
-        host_ip = value.first['HostIp']
-        host_port = value.first['HostPort']
-      end
-
-      entries << "#{container_port}:#{host_ip}:#{host_port}"
-    end
-
-    entries
-  end
-
-  #
-  # Generate a pair key:hash with
-  # format {service:label}
-  #
-  # The label will be the conainer name if not specified.
-  #
-  def self.format_links(links_array)
-    links = {}
-
-    return if links_array.nil?
-
-    links_array.each do |link|
-      parts = link.split(':')
-
-      case parts.length
+      case port_parts.length
+        # [container port]
         when 1
-          links[parts[0]] = parts[0]
+          ComposePort.new(container_port: port_parts[0])
 
+        # [host port]:[container port]
         when 2
-          links[parts[0]] = parts[1]
+          ComposePort.new(container_port: port_parts[0], host_port: port_parts[1])
+
+        # [host ip]:[host port]:[container port]
+        when 3
+          ComposePort.new(container_port: port_parts[0], host_port: port_parts[1], host_ip: port_parts[2])
       end
     end
 
-    links
+    #
+    # Format ports from running container
+    #
+    def format_ports_from_running_container(port_entry)
+      return [] if port_entry.nil?
+
+      # e.g. "8000/tcp" => "8000"
+      remove_protocol_from_container_port = -> (container_port) { container_port.gsub(/\D/, '').to_i }
+
+      port_entry.map do |container_port, host_mapping|
+        host_ip, host_port = '', ''
+        container_port = remove_protocol_from_container_port.call(container_port)
+
+        # Ports that are exposed but not published won't have a Host IP/Port,
+        # only a Container Port
+        unless host_mapping.nil?
+          host_ip   = host_mapping.first['HostIp']
+          host_port = host_mapping.first['HostPort']
+        end
+
+        "#{container_port}:#{host_ip}:#{host_port}"
+      end
+    end
+
+    #
+    # Generate a pair key:hash with
+    # format {service:label}
+    #
+    # The label will be the conainer name if not specified.
+    #
+    def format_links(links_array)
+      return nil if links_array.nil?
+
+      Hash[
+        links_array.map do |link|
+          service, label = link.split(':')
+          [service, label || service]
+        end
+      ]
+    end
+
+    #
+    # Generate a container name, based on:
+    # - directory where the compose file is saved;
+    # - container name (or label, if name isn't provided);
+    # - a sequential id;
+    #
+    def generate_container_name(container_name, container_label)
+      label = container_name || container_label
+      id    = next_available_container_id
+
+      "#{dir_name}_#{label}_#{id}"
+    end
   end
 
-  #
-  # Generate a container name, based on:
-  # - directory where the compose file is saved;
-  # - container name (or label, if name isn't provided);
-  # - a sequential index;
-  #
-  def self.generate_container_name(container_name, container_label)
-    label = container_name.nil? ? container_label : container_name
-    index = next_available_id
-
-    "#{@dir_name}_#{label}_#{index}"
-  end
-
-  private_class_method :next_available_id
+  private_class_method :next_available_container_id
 end
